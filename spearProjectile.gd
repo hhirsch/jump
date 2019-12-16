920663b86643e1b8998fa8f585963a0def8fd2cd
Henry Hirsch <henry@w3-net.de>
2019-12-16 18:45:20 +0100

    Decouple projectile and spear

--- spearProjectile.gd
+++ spearProjectile.gd
@@ -10,8 +10,10 @@ func _ready():
 	pass
 
 func reset():
+	var character = get_node("./../character/")
+	character.get_node("spear").visible = true
 	spearThrown = false
-	position = Vector2(0, 10)
+	#position = Vector2(0, 10)
 	disarm()
 
 func arm():
@@ -25,19 +27,28 @@ func disarm():
 	pass	
 	
 func _process(delta):
+	var character = get_node("./../character/")
 	var gamepads = Input.get_connected_joypads()
 	var gamepad = 0
 	if Input.get_connected_joypads().size() > 0:
 		gamepad = gamepads[Input.get_connected_joypads().size() -1]
 	var direction = Vector2(Input.get_joy_axis(gamepad, JOY_ANALOG_RX), Input.get_joy_axis(gamepad, JOY_ANALOG_RY))
-	var character = get_node("./../")
-	if direction.x > 0:
-		character.set_flip_h(false)
-	if direction.x < 0:
-		character.set_flip_h(true)		
-	var angle = direction.angle()
-	rotation = angle
+	if !spearThrown:
+		var angle = direction.angle()
+		rotation = angle
+		disarm()
+		visible = false
+	if spearThrown:
+		character.get_node("spear").visible = false
+		position += spearVector * 10
+		visible = true
+	if (Input.is_action_pressed("ui_shoot") && !spearThrown): 
+		position = character.position + Vector2(0, 10)
+		spearThrown = true
+		spearVector = direction
+		arm()
 	pass
+
 
 func _on_VisibilityNotifier2D_screen_exited():
 	reset()
--- zombie-collision.gd
+++ zombie-collision.gd
@@ -1,10 +1,10 @@
 extends CollisionShape2D
 
 func _on_Area2D_area_entered(area):
-	var zombie = get_tree().get_root().get_node("./game/zombie/zombie")
+	var zombie = get_node("./../../")
 	zombie.hide()
 	zombie.position = Vector2(0,575)
 	zombie.show()
-	var spear = get_tree().get_root().get_node("./game/character/character/spear")
+	var spear = get_tree().get_root().get_node("./game/character/spearProjectile")
 	spear.reset()
 	pass
