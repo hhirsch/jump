f62d5a8e90bd4509560851a73eb848bf1936bcdd
Henry Hirsch <henry@w3-net.de>
2019-12-17 20:54:45 +0100

    Add blood to zombies

---
+++ blood.gd
@@ -0,0 +1,13 @@
+extends Particles2D
+
+func _ready():
+	var timer = Timer.new()
+	add_child(timer)
+	timer.connect("timeout", self, "delete")
+	timer.set_wait_time(1.0)
+	timer.set_one_shot(true)
+	timer.start()
+	pass
+
+func delete():
+	queue_free()
--- zombie-collision.gd
+++ zombie-collision.gd
@@ -1,6 +1,6 @@
 extends CollisionShape2D
 var cookieCount
-
+var blood = preload("res://blood.tscn")
 func _ready():
 	cookieCount = 0
 	pass
@@ -13,5 +13,9 @@ func _on_Area2D_area_entered(area):
 		var cookie = get_tree().get_root().get_node("./game/cookies")
 		cookie.increase(1)
 		spear.reset()
+		var bloodNode = blood.instance()
+		bloodNode.set_name("blood")
+		bloodNode.position = zombie.position
+		get_tree().get_root().add_child(bloodNode)
 		zombie.queue_free()
 		pass
